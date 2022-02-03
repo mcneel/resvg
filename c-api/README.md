994c94f3c4a4afd27da7aaec3447174a1c0f40a8
Dan Rigdon-Bel <dan@mcneel.com>
2022-02-03 15:53:22 -0800

    Updates for Apple Silicon.  Cite RH-66987

--- c-api/README.md
+++ c-api/README.md
@@ -7,3 +7,13 @@ cargo build --release
 ```
 
 This will produce a dynamic C library that can be found at `../target/release`.
+
+## macOS Universal binary
+
+To build a macOS Universal binary that is compatible with Rhino, run:
+
+```
+./build_macos_universal.sh
+```
+
+and copy the *libresvg_rhino.dylib* to the rhino repository's */src4/rhino4/MacOS/Frameworks/* folder (overwriting the old one).  
---
+++ c-api/build_macos_universal.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# get the latest rustup
+rustup update
+
+# add the relevant platform targets
+rustup target add x86_64-apple-darwin
+rustup target add aarch64-apple-darwin
+
+# build for Intel
+cargo build --target=x86_64-apple-darwin --release
+
+# build for Apple Silicon
+cargo build --target=aarch64-apple-darwin --release
+
+cp ../target/x86_64-apple-darwin/release/libresvg_rhino.dylib libresvg_rhino_x86_64.dylib
+cp ../target/aarch64-apple-darwin/release/libresvg_rhino.dylib libresvg_rhino_arm64.dylib
+
+# Fix the rpaths
+install_name_tool -id @rpath/libresvg_rhino.dylib libresvg_rhino_x86_64.dylib
+install_name_tool -id @rpath/libresvg_rhino.dylib libresvg_rhino_arm64.dylib
+
+# lipo the libraries together
+lipo libresvg_rhino_x86_64.dylib libresvg_rhino_arm64.dylib -output libresvg_rhino.dylib -create
+
+# echo where this file should be copied
+if [ -f "libresvg_rhino.dylib" ]
+then
+rm libresvg_rhino_x86_64.dylib
+rm libresvg_rhino_arm64.dylib
+file libresvg_rhino.dylib
+echo "libresvg_rhino.dylib created - this file should be copied to src4/rhino4/MacOS/Frameworks (overwriting the older version)"
+fi
